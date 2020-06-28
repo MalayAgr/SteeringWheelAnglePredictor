18d386532fa48986a05484cccd226adc8fa16b05
Malay Agarwal <malay.agarwal261016@outlook.com>
2020-06-28 22:46:13 +0530

    Separate resizing, colorspace conversion, standardization and flipping into separate functions

--- processing.py
+++ processing.py
@@ -7,30 +7,46 @@ vectorized_imread = np.vectorize(cv2.imread,
 vectorized_imresize = np.vectorize(cv2.resize,
                                    excluded=['dsize', 'interpolation'],
                                    signature='(x,y,z)->(a,b,c)')
-vectorized_bgr2yuv = np.vectorize(cv2.cvtColor,
-                                  excluded='code',
-                                  signature="(x,y,z),()->(a,b,c)")
+vectorized_cvtColor = np.vectorize(cv2.cvtColor,
+                                   excluded='code',
+                                   signature="(x,y,z),()->(a,b,c)")
 
 
-def preprocess(images, size=(200, 66), epsilon=1e-7):
-    images = vectorized_imresize(images,
-                                 dsize=size,
-                                 interpolation=cv2.INTER_AREA)
-    images = vectorized_bgr2yuv(images, cv2.COLOR_BGR2YUV)
+def resize_images(images, size=(200, 66)):
+    return vectorized_imresize(
+        images, dsize=size, interpolation=cv2.INTER_AREA
+    )
 
+
+def convert_images_colorspace(images, colorspace=cv2.COLOR_BGR2YUV):
+    return vectorized_cvtColor(images, colorspace)
+
+
+def channelwise_standardization(images, epsilon=1e-7):
     mean = np.mean(images, axis=(1, 2), keepdims=True)
     std = np.std(images, axis=(1, 2), keepdims=True)
-    images = (images - mean) / (std + epsilon)
+    return (images - mean) / (std + epsilon)
 
+
+def preprocess(
+    images, size=(200, 66), epsilon=1e-7, colorspace=cv2.COLOR_BGR2YUV
+):
+    images = resize_images(images, size=size)
+    images = convert_images_colorspace(images, colorspace=colorspace)
+    images = channelwise_standardization(images, epsilon=epsilon)
     return images.astype(np.float32)
 
 
-def augment_images(images, labels):
-    to_be_augmented = (np.random.rand(len(images)) < 0.6)
-
-    to_be_flipped = (np.random.rand(len(images)) < 0.5)
-    mask = to_be_flipped & to_be_augmented
-
+def flip_images(images, labels, mask, threshold=0.5):
+    to_be_flipped = (np.random.rand(len(images)) < threshold)
+    mask &= to_be_flipped
     images[mask] = np.flipud(images[mask])
     labels[mask] = -labels[mask]
     return images, labels
+
+
+def augment_images(images, labels, aug_threshold=0.6, flip_threshold=0.5):
+    to_be_augmented = (np.random.rand(len(images)) < aug_threshold)
+    return flip_images(
+        images, labels, to_be_augmented, threshold=flip_threshold
+    )
